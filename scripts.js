605e674ed1f284eb663db2b9ce43c8f7c344cf6c
SayemImran <ik262142@gmail.com>
2026-03-08 08:15:47 +0600

    initial file added

---
+++ scripts.js
@@ -0,0 +1,12 @@
+const checker = (event) => {
+  event.preventDefault();
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+
+  if (username === "admin" && password === "admin123") {
+    alert("Success!!");
+    window.location.href = "./apiPage.html";
+  } else {
+    alert("Credential Error");
+  }
+};
